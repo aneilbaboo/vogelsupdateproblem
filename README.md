ba7ad911ef7406ec70fe4c8e270f82f6a82568c1
Aneil Mallavarapu <aneilmallavar@gmail.com>
2017-02-27 19:34:35 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Vogels update problem
+
+```
+DYNAMOLIB=/path/to/dynamolib java -Djava.library.path=${DYNAMOLIB}/DynamoDBLocal_lib -jar ${DYNAMOLIB}/DynamoDBLocal.jar -inMemory"
+
+npm start
+```
---
+++ index.js
@@ -0,0 +1,58 @@
+var vogels = require('vogels');
+var joi = require('joi');
+// const AWS = vogels.AWS;
+
+var vogels = require('vogels');
+vogels.AWS.config.update({
+  accessKeyId: 'AKID',
+  secretAccessKey: 'SECRET',
+  region: "REGION",
+  "endpoint": 'http://localhost:8000'
+});
+// var dynamoDriver = vogels.dynamoDriver(new AWS.DynamoDB({
+//   endpoint: 'http://localhost:8000',
+//   accessKeyId: 'dynamo',
+//   region: 'test',
+//   apiVersion: '2012-08-10',
+//   secretAccessKey: 'testDummyKey'
+// }));
+//
+// dynamoDriver.config.credentials = new AWS.Credentials({
+//   accessKeyId: "fakekey",
+//   secretAccessKey: "fakesecret",
+//   region: "fakeregion"
+// });
+
+var TestModel = vogels.define('TestModel', {
+  hashKey : 'id',
+
+  // add the timestamp attributes (updatedAt, createdAt)
+  timestamps : true,
+
+  schema : {
+    id      : joi.string(),
+    num     : joi.number()
+  }
+});
+
+vogels.createTables(function(err) {
+  if (err) {
+    console.log('Error creating tables: ', err);
+  } else {
+    console.log('Tables have been created');
+
+    var test = new TestModel({id: "1"});
+
+    test.save(function (err, tSaved) {
+      console.log("Attempting to update num to 100");
+      tSaved.update({num: 100}, function (err, tUpdated) {
+        if (tUpdated.get('num')==100) {
+          console.log("Successfully updated test.num => 100!");
+        } else {
+          console.log("Failed to update test instance: %j", tUpdated.attrs);
+        }
+
+      });
+    });
+  }
+});
